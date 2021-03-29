4309b8753f1a1bcf755d558d05918c447efbdd48
yarimv <39723219+yarimv@users.noreply.github.com>
2021-03-29 17:37:39 +0900

    Add files via upload

---
+++ bookclub.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>책클럽</title>
+  </head>
+  <body>
+
+
+   <h2><a href="modify.php">책수정하기</a></h2>
+   <h2><a href="request.php">신청하기</a></h2>
+
+   
+   <h2></h2>
+  </body>
+</html>
---
+++ input_list.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>책입력하기</title>
+  </head>
+  <body>
+    안녕하세요, <?php echo $_GET['id']; ?>님
+  </body>
+</html>
